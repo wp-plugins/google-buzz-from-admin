fba89413c2045058744d989a12884d38080aeb68
abbas.s786 <abbas.s786@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-25 15:34:35 +0000

    Version 1
    git-svn-id: https://plugins.svn.wordpress.org/google-buzz-from-admin/trunk@210202 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ GoogleBuzzAdmin.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: Google Buzz from Admin
+Plugin URI: 
+Description: This plugin Lets you Google Buzz posts from admin Page
+Author: Abbas Suterwala
+Version: 1.0
+Author URI: http://www.abbassuterwala.com 
+*/
+
+/* Use the admin_menu action to define the custom boxes */
+add_action('admin_menu', 'GBuzzAdmin_add_custom_box');
+
+
+
+/* Adds a custom section to the "advanced" Post  edit screens */
+
+function GBuzzAdmin_add_custom_box() {
+
+  if( function_exists( 'add_meta_box' )) 
+  {
+    add_meta_box( 'GBuzzAdmin_sectionid', 'Google Buzz from Admin', 'GBuzzAdmin_inner_add_custom_box', 'post', 'side' );
+  } 
+}
+
+   
+/* 
+This function retrieves the last Post which is save in Wordpress and 
+Gives a link which one can use to publish to Google Buzz
+*/
+
+function GBuzzAdmin_inner_add_custom_box() 
+{
+
+	$recentposts =  wp_get_recent_posts( 1 );
+	$latestpost = $recentposts[0];
+	//print_r ($recentposts);
+	$latestPostTitle = $latestpost['post_title'];
+
+	$latestPostPermalink = get_permalink( $latestpost['ID'] );
+
+	echo '<input type="hidden" name="GBuzzAdmin_noncename" id="GBuzzAdmin_noncename" value="' . 
+	wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+
+	echo 'The last post saved is <br><strong>'.$latestPostTitle . '</strong> <br>Do you want to Google<br>';
+
+	echo  '<a href="http://www.google.com/reader/link?url='.$latestPostPermalink.'&title='.str_replace(' ','+',$latestPostTitle).'&srcURL='.get_bloginfo( 'url' ).'" target="_blank" ">Buzz This</a>';
+
+}
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Browser Counter ===
+Contributors: Abbas Suterwala
+Donate link:http://www.abbassuterwala.com
+Tags: Google Buzz,Buzz,Google,Share,Socail Networking,Bookmarking
+Requires at least: 2.9.1
+Tested up to: 2.9.1
+Stable tag: 4.3
+
+This plugin retrives the last post you saved/published and 
+directly from the Post edit page allows you to share it with others on your Google Buzz
+
+== Description ==
+
+This plugin retrives the last post you saved/published and 
+directly from the Post edit page allows you to share it with others on your Google Buzz
+
+== Installation ==
+
+1. Upload Folder 'GoogleBuzzAdmin' to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. You are Ready :)
+
+== Frequently Asked Questions ==
+
+= What does this plug in do ? =
+
+This plugin retrives the last post you saved/published and 
+directly from the Post edit page allows you to share it with others on your Google Buzz
+
+== Screenshots ==
+1. No Screeshots as yet :(
+== Changelog ==
+
+= 1.0 =
+* First Version of the plugin
+
+== Upgrade Notice ==
+None
+
+
